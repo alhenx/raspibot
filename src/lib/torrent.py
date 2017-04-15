ae91e5659a6199821f773d98e27eccc20b0f4ace
Alhen <alhenx@gmail.com>
2017-04-15 18:12:52 +0200

    delete torrent with buttons

--- src/lib/torrent.py
+++ src/lib/torrent.py
@@ -5,16 +5,27 @@
 import os
 import json
 
-jsonFile = "/opt/raspibot/"
+jsonFile = os.getcwd()
 
-with open(jsonFile+'data.json') as json_data:
+with open(jsonFile+'/data.json') as json_data:
 	d = json.load(json_data)
 	userTorrent = d["torrent-user"]
 	passTorrent = d["torrent-pass"]
 
-def getList():
-	cmd = "transmission-remote -n "+userTorrent+":"+passTorrent+" -l | sed -e \'1d;$d;s/^ *//\' | awk \'{print $1 \"\\t\" substr($0,index($0,$10))\"\\n\"}\'"
-	return os.popen(cmd).read()
+def getList(type):
+	cmd = "transmission-remote -n "+userTorrent+":"+passTorrent+" -l |sed -e \'s/ kB\| MB\| GB\| TB//g\'|sed -e \'1d;$d;s/^ *//\'| awk \'{print $1 \"\\t\" substr($0,index($0,$9))}\'"
+	responseTransmission = os.popen(cmd).read()
+	listTorrent = responseTransmission.splitlines()
+	matrixTorrent = [[] for i in range(len(listTorrent))]
+	for i in range(len(listTorrent)):
+		matrixTorrent[i] = listTorrent[i].split("\t")
+	if type==0:
+		stringTorrent = ""
+		for i in range(len(listTorrent)):
+			stringTorrent+=matrixTorrent[i][1]+"\n\n"
+		return stringTorrent
+	if type==1:
+		return matrixTorrent
 
 def addTorrent(torrentLink):
 	cmd = "transmission-remote -n "+userTorrent+":"+passTorrent+" -a \""+torrentLink+"\""
--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 #
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 import logging
 import json
+import os
 from lib import stats, torrent
 
 # Enable logging
@@ -13,7 +15,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-jsonFile = "/opt/raspibot/"
+jsonFile = os.getcwd()
+
+def build_menu(buttons,
+               n_cols: int):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    return menu
 
 def chatid(bot, update):
 	update.message.reply_text(quote=True, text=update.message.chat_id)
@@ -22,13 +29,26 @@ def getStats(bot, update):
 	update.message.reply_text(quote=True, text="<code>"+stats.stats()+"</code>", parse_mode="HTML")
 
 def getTorrentList(bot, update):
-	update.message.reply_text(quote=True, text="<code>"+torrent.getList()+"</code>", parse_mode="HTML")
+	update.message.reply_text(quote=True, text="<code>"+str(torrent.getList(0))+"</code>", parse_mode="HTML")
 
 def addTorrent(bot, update):
 	update.message.reply_text(quote=True, text=torrent.addTorrent(update.message.text[12:]))
-
+	
 def delTorrent(bot, update):
-	update.message.reply_text(quote=True, text=torrent.delTorrent(update.message.text[12:]))
+	matrixTorrent=torrent.getList(1)
+	button_list = []
+	for i in range(0, len(matrixTorrent)):
+		button_list.append(InlineKeyboardButton(text=matrixTorrent[i][1], callback_data="del"+str(matrixTorrent[i][0])))
+	update.message.reply_text(quote=True, text="TESTING DELETE TORRENT", reply_markup=InlineKeyboardMarkup(build_menu(button_list, n_cols=1)))
+
+def button(bot, update):
+	query = update.callback_query
+
+	if query.data.startswith("del"):
+		torrent.delTorrent(query.data[3:])
+		bot.editMessageText(text="Torrente deleted",
+							chat_id=query.message.chat_id,
+							message_id=query.message.message_id,)
 
 def response(bot, update):
 	if (update.message.text=="hola"):
@@ -39,7 +59,7 @@ def error(bot, update, error):
 
 
 def main():
-	with open(jsonFile+'data.json') as json_data:
+	with open(jsonFile+'/data.json') as json_data:
 		d = json.load(json_data)
 		updater = Updater(d["token"])
 
@@ -55,6 +75,8 @@ def main():
 	dp.add_handler(CommandHandler("torrentlist", getTorrentList))
 	dp.add_handler(CommandHandler("torrentadd", addTorrent))
 	dp.add_handler(CommandHandler("torrentdel", delTorrent))
+	dp.add_handler(CallbackQueryHandler(button))
+
 
 	# Response
 	dp.add_handler(MessageHandler(Filters.text, response))
