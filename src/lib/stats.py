f53a139091d177b8edce268d8f30235a0cac5a51
Raúl Díez <rauldiez20@gmail.com>
2017-05-07 22:53:14 +0200

    Updated aliases output. Fixed stats text.

--- src/lib/stats.py
+++ src/lib/stats.py
@@ -5,5 +5,5 @@
 import os
 
 def stats():
-	cmd = "echo \"CPU stats:\\n#############\"; top -bn1 -o%CPU |head -12|tail -6|awk \'{ printf(\"%-8s %-8s %-8s %-8s %-8s\\n\", $1, $2, $9, $10, $12); }\';echo \"\\nRAM stats:\\n#############\";echo \"(MB) \\t\\t Total \\t\\t Used \\t\\t Free\";free -m|head -2|tail -1|awk \'{ printf(\"%-8s %-8s %-8s %-8s\\n\", $1, $2, $3, $4); }\';echo \"\\nHDD stats:\\n#############\";df -h;echo \"\\nOther stats:\\n#############\";/opt/vc/bin/vcgencmd measure_temp|sed \'s/temp=/Temperature: /g\'|sed \"s/\'/º/g\";echo \"\\nUptime: \";uptime -p"
+	cmd = "echo \"CPU stats:\\n#############\"; top -bn1 -o%CPU |head -12|tail -6|awk \'{ printf(\"%-8s %-8s %-8s %-8s %-8s\\n\", $1, $2, $9, $10, $12); }\';echo \"\\nRAM stats:\\n#############\";echo \"(MB) \\t\\t Total \\t\\t Used \\t\\t Free\";free -m|head -2|tail -1|awk \'{ printf(\"%-8s %-8s %-8s %-8s\\n\", $1, $2, $3, $4); }\';echo \"\\nHDD stats:\\n#############\";df -h;echo \"\\nOther stats:\\n#############\";/opt/vc/bin/vcgencmd measure_temp|sed \'s/temp=/Temperature: /g\'|sed \"s/\'/º/g\";uptime -p|sed -e \'s/\\(.*\\)/Uptime: \\1/g\'"
 	return os.popen(cmd).read()
